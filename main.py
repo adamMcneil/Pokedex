844ddb866dd5739bd26bb2a9c386356809d8ec29
Adam McNeil <adam.mcneil@oasisdigital.com>
2022-12-10 12:46:38 -0600

    updated and cleaned main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
 from multiprocessing.connection import Connection
 import mysql.connector
 from mysql.connector import Error
-# import pandas as pd
+
+# connects to the server and returns the connection for later queries
 
 
 def create_server_connection(host_name, user_name, user_password) -> Connection:
@@ -18,11 +18,12 @@ def create_server_connection(host_name, user_name, user_password) -> Connection:
         print(f"error: '{err}'")
     return connection
 
+# use this to get the results of sql query return as a list where query is just a string like a normal sql query
 
-def make_sql_request(connection: Connection, query: str):
+
+def make_sql_query(connection: Connection, query: str):
     cursor = connection.cursor()
     try:
-        cursor.execute("use pokedex")
         cursor.execute(query)
         result = cursor.fetchall()
         print("query successful")
@@ -32,17 +33,20 @@ def make_sql_request(connection: Connection, query: str):
     return
 
 
-pw = "Pokemon2022!"
+# it is the password you set up when you install mysql
+password = "Pokemon2022!"
+# database you want to use
+database = "pokedex"
 
-db = "pokedex"
+query = "select * from pokemon where overall_stat < 200 order by overall_stat;"
 
-query = "select * from pokemon;"
+# connect to the server
+connection = create_server_connection("localhost", "root", password)
 
+# specify you are using the pokedex database on the server
+make_sql_query(connection, "USE " + database)
 
-connection = create_server_connection("localhost", "root", pw)
-
-cursor = connection.cursor()
-cursor.execute("use pokedex")
-
-result = make_sql_request(connection, query)
-print(result)
+# then store the results of the query in result and print the list, results.
+results = make_sql_query(connection, query)
+for result in results:
+    print(result)
